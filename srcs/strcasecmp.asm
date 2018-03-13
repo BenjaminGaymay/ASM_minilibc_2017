58d711f0542ef23be68ee74855c4e50c6eaee361
Benjamin GAYMAY <benjamin.gaymay@epitech.eu>
2018-03-13 13:24:37 +0100

    correction strcasecmp

--- srcs/strcasecmp.asm
+++ srcs/strcasecmp.asm
@@ -14,22 +14,26 @@ while:	mov al, byte[rdi + rcx]
 	jz diff
 	cmp bl, 0
 	jz diff
-	jmp isUpp
 
-alUpp:	cmp al, 123
-	jae isUpp
+alUpp:	cmp al, 97
+	jb blUpp
+	cmp al, 123
+	jae blUpp
+	cmp bl, 65
+	jb blUpp
+	cmp bl, 90
+	jae blUpp
 	sub al, 32
-	jmp isUpp
 
-blUpp:	cmp bl, 123
+blUpp:	cmp bl, 97
+	jb comp
+	cmp bl, 123
+	jae comp
+	cmp al, 65
+	jb comp
+	cmp al, 90
 	jae comp
 	sub bl, 32
-	jmp isUpp
-
-isUpp:	cmp al, 97
-	jae alUpp
-	cmp bl, 97
-	jae blUpp
 
 comp:	cmp al, bl
 	jne diff
